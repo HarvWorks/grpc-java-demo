fbebc31c6a3372c015cadf313a2f763c3de095e4
Harvey Chui <harvey.chui1@t-mobile.com>
2019-10-18 15:07:30 -0700

    Got the client working

--- src/main/java/com/harvworks/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/harvworks/grpc/greeting/client/GreetingClient.java
@@ -1,6 +1,10 @@
 package com.harvworks.grpc.greeting.client;
 
 import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -9,15 +13,34 @@ public class GreetingClient {
         System.out.println("Hello I am grpc client");
 
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
                 .build();
 
         System.out.println("Creating Stub");
-        DummyServiceGrpc.DummyServiceBlockingStub syncClient  = DummyServiceGrpc.newBlockingStub(channel);
 
+        // Old Dummy
+        // DummyServiceGrpc.DummyServiceBlockingStub syncClient  = DummyServiceGrpc.newBlockingStub(channel);
         // async client
         // DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
 
-        // do something here
+        // Created greet client service (Sync)
+        GreetServiceGrpc.GreetServiceBlockingStub greetSyncClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        // Created a protocol buffer greeting message
+        Greeting greeting = Greeting.newBuilder()
+                .setFirstName("Hi")
+                .setLastName("Bellow")
+                .build();
+
+        // Do the same for greetResponse
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        // call the RPC and get a greetResponse
+        GreetResponse greetResponse =  greetSyncClient.greet(greetRequest);
+
+        System.out.println(greetResponse.getResult());
 
         System.out.println("Shutting down channel");
         channel.shutdown();
