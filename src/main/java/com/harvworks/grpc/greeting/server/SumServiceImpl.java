470c28426e84e9fc5ea7c4c5941c907187ef86da
Harvey Chui <harvey.chui1@t-mobile.com>
2019-10-18 16:26:46 -0700

    Starting the SumServiceImpl

--- src/main/java/com/harvworks/grpc/greeting/server/SumServiceImpl.java
+++ src/main/java/com/harvworks/grpc/greeting/server/SumServiceImpl.java
@@ -1,4 +1,13 @@
 package com.harvworks.grpc.greeting.server;
 
-public class SumServiceImpl {
+import com.proto.sum.SumRequest;
+import com.proto.sum.SumResponse;
+import com.proto.sum.SumServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class SumServiceImpl extends SumServiceGrpc.SumServiceImplBase {
+    @Override
+    public void sum(SumRequest request, StreamObserver<SumResponse> responseObserver) {
+
+    }
 }
