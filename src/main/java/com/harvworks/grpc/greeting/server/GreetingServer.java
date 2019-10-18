e04d87cd7c8cdc962b6b9812112607594e877e59
Harvey Chui <harvey.chui1@t-mobile.com>
2019-10-18 14:10:23 -0700

    Added code to code for server boiler plate

---
+++ src/main/java/com/harvworks/grpc/greeting/server/GreetingServer.java
@@ -0,0 +1,25 @@
+package com.harvworks.grpc.greeting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello world");
+
+        Server server = ServerBuilder.forPort(50051)
+                .build();
+
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received system shutdown");
+            server.shutdown();
+            System.out.println("Successfully shut down");
+        }));
+
+        server.awaitTermination();
+    }
+}
