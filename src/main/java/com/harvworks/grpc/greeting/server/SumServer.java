3f95971b6bb1d9e32e5c82559b5a827e93236404
Harvey Chui <harvey.chui1@t-mobile.com>
2019-10-18 16:25:35 -0700

    Built the server

--- src/main/java/com/harvworks/grpc/greeting/server/SumServer.java
+++ src/main/java/com/harvworks/grpc/greeting/server/SumServer.java
@@ -1,4 +1,26 @@
 package com.harvworks.grpc.greeting.server;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
 public class SumServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello I am the grpc server");
+
+        Server server = ServerBuilder.forPort(50051)
+                .addService(new SumServiceImpl())
+                .build();
+
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received system shutdown");
+            server.shutdown();
+            System.out.println("Successfully shut down");
+        }));
+
+        server.awaitTermination();
+    }
 }
